aa5795a2f7803b7a94c90826e88ec8b8067ff4d4
BluePrinceEB <primeworld010@mail.ru>
2017-01-10 19:41:15 +0300

    Update FlashKappa.lua

--- FlashKappa.lua
+++ FlashKappa.lua
@@ -3,7 +3,7 @@ Config:Boolean("range", "Draw Range", true)
 Config:Boolean("circle", "Draw Circle", true)
 
 OnDraw(function()
-	local Flash_Pos = myHero.pos + (Vector(GetMousePos()) -  myHero.pos):normalized() * 475
+	local Flash_Pos = myHero.pos + (Vector(GetMousePos()) -  myHero.pos):normalized() * 425
 	if Config.circle:Value() then DrawCircle(Flash_Pos, 75, 1, 1, GoS.White) end
-	if Config.range:Value() then DrawCircle(GetOrigin(myHero), 475, 1, 1, GoS.White) end
+	if Config.range:Value() then DrawCircle(GetOrigin(myHero), 425, 1, 1, GoS.White) end
 end)
